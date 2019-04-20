b9360c6fd458af8a330528830e3fabffab4c32e6
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-04-20 12:32:22 +0100

    Extracted dashboard card component

---
+++ components/DashboardCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-col cols="12" class="mb-5">
+        <b-card class="card-body bg-secondary rounded-lg border-0">
+            <h5 class="text-capitalize dashboard-card-title">{{title}}</h5>
+            <b-col cols="12" class="py-4 px-0">
+                <b-row>
+                    <slot></slot>
+                </b-row>
+            </b-col>
+        </b-card>
+    </b-col>
+</template>
+
+<script>
+export default {
+    name: 'dashboard-card',
+    props: ['title']
+}
+</script>
+
+<style>
+.dark-mode .dashboard-card-title{
+    color: white;
+}
+</style>
