dbd2665d2e2f74e3b181713fedaa17335a90282c
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-04-20 12:32:47 +0100

    Registered components

--- plugins/components.js
+++ plugins/components.js
@@ -4,8 +4,11 @@ import ProgressCard from "@/components/ProgressCard.vue"
 import FigureCard from "@/components/FigureCard.vue"
 import DataTable from '@/components/Datatable.vue'
 import PieCard from '@/components/PieCard.vue'
+import DashboardCard from '@/components/DashboardCard.vue'
 
 Vue.component('DataTable', DataTable)
 Vue.component('ProgressCard', ProgressCard)
 Vue.component('FigureCard', FigureCard)
 Vue.component('PieCard', PieCard)
+Vue.component('DashboardCard', DashboardCard)
+
