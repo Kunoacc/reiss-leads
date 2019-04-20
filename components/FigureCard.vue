6baf2042b7b71fc65b34fc5604a891e9a667d74b
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-04-20 13:37:11 +0100

    Added secondary support to figure card

--- components/FigureCard.vue
+++ components/FigureCard.vue
@@ -1,6 +1,6 @@
 <template>
     <b-col cols="12" md="6" lg="3" class="d-flex flex-column pt-3 pt-lg-0 figure-card">
-        <b-card class="card-body shadow">
+        <b-card :class="['card-body', 'shadow', {'bg-secondary border-0': isSecondary && !$store.state.isThemeDark || false}]">
             <b-row class="text-center mx-auto justify-content-center">
                 <h4 class="text-primary font-weight-bold text-center col-12 figure-title">{{title}}</h4>
                 <p class="text-center text-capitalize text-sm">
@@ -14,7 +14,7 @@
 <script>
 export default {
  name: 'figure-card',
- props: ['title']
+ props: ['title', 'isSecondary']
 }
 </script>
 
