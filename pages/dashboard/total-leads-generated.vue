47c8fa88a25c5ef37fb2b6ccba3cfa3e4a9a3f93
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2019-04-20 12:31:52 +0100

    Completed Pages

--- pages/dashboard/total-leads-generated.vue
+++ pages/dashboard/total-leads-generated.vue
@@ -1,26 +1,101 @@
 <template>
      <div class="row my-5">
-        <b-col cols="12 mb-5">
-            <b-card class="card-body bg-secondary rounded-lg border-0">
-                <h5>Summary</h5>
-                    <b-col cols="12" class="py-4 px-0">
-                        <b-row>
-                            <div class="col-12 col-md-6">
-                                <data-table
-                                    api-url="https://vuetable.ratiw.net/api/users"
-                                    :fields="fields"
-                                    :per-page="6"
-                                    :data="data"
-                                    :isSearchable="false"
-                                ></data-table>
-                            </div>
-                            <div class="col-12 col-md-6">
-                                <pie-card></pie-card>
-                            </div>
-                        </b-row>
-                    </b-col>
-            </b-card>
-        </b-col>
+         <dashboard-card title="summary">
+            <b-col cols="12" class="py-4 px-0">
+                <b-row>
+                    <div class="col-12 col-lg-6">
+                        <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <pie-card title="total amount converted" chart-title="5000 leads"></pie-card>
+                    </div>
+                </b-row>
+            </b-col>
+            <b-col cols="12" class="py-4 px-0">
+                <b-row>
+                    <div class="col-12 col-lg-6">
+                        <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <pie-card title="total amount converted" chart-title="5000 leads"></pie-card>
+                    </div>
+                </b-row>
+            </b-col>
+         </dashboard-card>
+
+         <dashboard-card title="all leads">
+             <div class="row">
+                 <div class="col-12">
+                     <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                 </div>
+                 <div class="col-12 col-lg-6 my-5">
+                     <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                 </div>
+             </div>
+         </dashboard-card>
+
+         <dashboard-card title="pipeline by source">
+             <div class="row">
+                 <div class="col-12 col-lg-6">
+                     <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                 </div>
+                 <div class="col-12 col-lg-6">
+                     <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                 </div>
+                 <div class="col-12 my-5">
+                     <data-table
+                            api-url="https://vuetable.ratiw.net/api/users"
+                            :fields="fields"
+                            :per-page="6"
+                            :data="data"
+                            :isSearchable="false"
+                            :class="['rounded', {'shadow': $store.state.isThemeDark}]"
+                        ></data-table>
+                 </div>
+             </div>
+         </dashboard-card>
      </div>
 </template>
 
@@ -29,14 +104,15 @@ export default {
     name: 'total-leads-generated',
     data(){
         return {
-            fields: ['id',
-            {name: 'name', title: 'Full Name', sortField: 'name'},
-            {name: 'direction'},
-            {name: 'type'},
-            {name: 'from', sortField: 'from'},
-            {name: 'to', sortField: 'to'},
-            {name: 'extension'},
-            // 'actions'
+            fields: [
+                'id',
+                {name: 'name', title: 'Full Name', sortField: 'name'},
+                {name: 'direction'},
+                {name: 'type'},
+                {name: 'from', sortField: 'from'},
+                {name: 'to', sortField: 'to'},
+                {name: 'extension'},
+                // 'actions'
             ],
             data: {}
         }
---
+++ pages/dashboard/total-web-enquiries.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'total-web-enquiries',
+    data(){
+        return {
+            
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
